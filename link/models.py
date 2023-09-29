d62ce5734acde71ed0f496ea24c15e7cb358c7bd
busayo <ibrahimbusay@gmail.com>
2023-09-29 22:11:56 +0100

    create my model.py

--- link/models.py
+++ link/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    Video_file = models.FileField(upload_to='Video/')
+    upload_data = models.DateTimeField(auto_now_add=True)
