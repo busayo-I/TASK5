bf04fab3b61f9b51a22c8712fb5a94b4a35e0364
busayo <ibrahimbusay@gmail.com>
2023-09-30 00:31:18 +0100

    create my urls.py and view.py with some  update in  my model

--- link/models.py
+++ link/models.py
@@ -7,3 +7,6 @@ class Video(models.Model):
     title = models.CharField(max_length=255)
     Video_file = models.FileField(upload_to='Video/')
     upload_data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- link/views.py
+++ link/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import responses
+from .models import Video
+from .serializer import VideoSerializer
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def create(self, request, *args, **kwargs):
