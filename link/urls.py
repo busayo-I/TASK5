e1bc8fe2e942f77759bb1dde8087a04eac562317
busayo <ibrahimbusay@gmail.com>
2023-10-01 13:51:18 +0100

    working on  my view.py

--- link/urls.py
+++ link/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import VideoViewSet
-
-router = DefaultRouter()
-router.register(r'video', VideoViewSet)
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('create_video/', views.CreateVideo.as_view(), name='create_video'),
+    path('add_video_chunk/<int:video_id>/', views.AddVideoChunk.as_view(), name='add_video_chunk'),
+    path('complete_video/<int:video_id>/', views.CompleteVideo.as_view(), name='complete_video'),
+    path('stream_video/<int:video_id>/', views.StreamVideo.as_view(), name='stream_video'),
 ]
--- link/views.py
+++ link/views.py
@@ -4,98 +4,109 @@ from rest_framework import status
 from .models import Video, VideoChunk
 from .serializer import VideoSerializer
 from rest_framework.request import Request
-from rest_framework.parsers import MultiPartParser, FormParser
 from django.conf import settings
 import os
 from django.http import StreamingHttpResponse, FileResponse
 from django.shortcuts import get_object_or_404
 
-class Status(APIView):
-    def get(self, request):
-        return Response({"status": "ok"}, status=status.HTTP_200_OK)
-    
-class CreateVideoList(APIView):
-    parser_classes = (MultiPartParser, FormParser)
-    
-    def post(self, requst: Request):
-        data = requst.data()
-        video = data.get("video")
-        title = data.get("title")
-        description = data.get("description")
-
-        if video is None:
-            response = {
-                "Status": "Error",
-                "Massage": "Video file is required.",
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        serializer = VideoSerializer(data=data)
+class CreateVideo(APIView):
+    def post(self, request: Request):
+        serializer = VideoSerializer(data={})
         if serializer.is_valid():
             video_obj = serializer.save()
-            video_url = 
-            video_obj.video_url = video_url
-            video_obj.save()
-
-            try:
-                chunk_directory = os.path.join(settings.MEDIA_ROOT, 'video_chunks', str(video_obj.id))
-                os.makedirs(chunk_directory, exist_ok=True)
-                chunk_number = 0
-
-                while True:
-                    chunk = video.read(1024 * 1024)
-
-                    if chunk is None:
-                        break
-
-                    chunk_filename = f"chunk_{chunk_number}.mp4"
-                    chunk_path = os.path.join(chunk_directory, chunk_filename)
-
-                    with open(chunk_path, "wb") as chunk_file:
-                        chunk_file.write(chunk)
-                    #creating a video chunk object
-                    video_chunk = VideoChunk.objects.create(
-                        video=video_obj,
-                        chunk_number=chunk_number,
-                        chunk_file=chunk_path
-                    )
-                    video_chunk.save()
-                    chunk_number = chunk_number + 1
-            except Exception as e:
-                response = {
-                    "Status": "Error",
-                    "Massage": "Error while uploading video chunks.",
-                    "data": str(e)
-                }
-                return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             response = {
-                "Status": "Success",
-                "Massage": "Video uploaded successfully.",
-                "data": serializer.data
+                "status": "success",
+                "message": "Video created successfully",
+                "video_id": video_obj.id
             }
             return Response(response, status=status.HTTP_201_CREATED)
         else:
             response = {
-                "Status": "Error",
-                    "Massage": "Video not created.",
-                    "data": serializer.errors
+                "status": "error",
+                "message": "Video not created",
+                "data": serializer.errors
             }
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
-        
+class AddVideoChunk(APIView):
+    def post(self, request: Request, video_id):
+        video_chunk = request.data.get('video_chunk')
+        chunk_number = request.data.get('chunk_number')
 
+        if not video_chunk or chunk_number is None:
+            response = {
+                "status": "error",
+                "message": "Video chunk and chunk number are required.",
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
-# class VideoViewSet(viewsets.ModelViewSet):
-#     queryset = Video.objects.all()
-#     serializer_class = VideoSerializer
+        try:
+            video_obj = Video.objects.get(pk=video_id)
+        except Video.DoesNotExist:
+            return Response({"status": "error", "message": "Video not found."}, status=status.HTTP_404_NOT_FOUND)
 
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-#     def retrieve(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         serializer = self.get_serializer(instance)
-#         return Response(serializer.date) 
+        try:
+            chunk_directory = os.path.join(settings.MEDIA_ROOT, 'video_chunks', str(video_id))
+            os.makedirs(chunk_directory, exist_ok=True)
+            chunk_filename = f'chunk_{chunk_number}.mp4'
+            chunk_path = os.path.join(chunk_directory, chunk_filename)
+
+            with open(chunk_path, 'wb') as chunk_file:
+                chunk_file.write(video_chunk.read())
+
+            # Create a VideoChunk object for this chunk
+            video_chunk = VideoChunk.objects.create(
+                video=video_obj,
+                chunk_number=chunk_number,
+                chunk_file=chunk_path
+            )
+            video_chunk.save()
+
+            response = {
+                "status": "success",
+                "message": "Video chunk uploaded successfully",
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            response = {
+                "status": "error",
+                "message": "Error uploading video chunk.",
+                "data": str(e)
+            }
+            return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class CompleteVideo(APIView):
+    def post(self, request: Request, video_id):
+        try:
+            video_obj = Video.objects.get(pk=video_id)
+        except Video.DoesNotExist:
+            return Response({"status": "error", "message": "Video not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        response_message = "Video marked as complete"
+        video_obj.completed = True
+        video_obj.save()
+
+        response = {
+            "status": "success",
+            "message": response_message,
+        }
+        return Response(response, status=status.HTTP_200_OK)
+
+class StreamVideo(APIView):
+    def get(self, request: Request, video_id):
+        try:
+            video = Video.objects.get(pk=video_id)
+        except Video.DoesNotExist:
+            return Response({"status": "error", "message": "Video not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        chunk_files = VideoChunk.objects.filter(video=video).order_by('chunk_number')
+
+        def generate_chunks():
+            for chunk_file in chunk_files:
+                with open(chunk_file.chunk_file.path, 'rb') as file:
+                    for chunk in file:
+                        yield chunk
+
+        response = StreamingHttpResponse(generate_chunks(), content_type='video/mp4')
+        response['Content-Disposition'] = f'inline; filename="{video.title}.mp4"'
+        return response
