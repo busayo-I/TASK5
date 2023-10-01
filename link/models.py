f83b730819b2b9246be2d51ffdedb6aa62e12efb
busayo <ibrahimbusay@gmail.com>
2023-10-01 16:01:42 +0100

    updating

--- link/models.py
+++ link/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Video(models.Model):
     title = models.CharField(max_length=255)
-    Video_file = models.FileField(upload_to='Video/')
-    upload_data = models.DateTimeField(auto_now_add=True)
-
-class VideoChunk(models.Model):
-    video = models.ForeignKey('Video', related_name='chunks', on_delete=models.CASCADE)
-    chunk_number = models.PositiveBigIntegerField()
-    chunk_file = models.FileField(upload_to='video_chunks/')
-
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
-        return f'Chunk {self.chunk_number} of {self.video.title}'
+
+class VideoChunk(models.Model):
+    video = models.ForeignKey(Video, related_name='chunks', on_delete=models.CASCADE)
+    chunk_number = models.IntegerField()
+    chunk_file = models.FileField(upload_to='video_chunks/')
+
+    def __str__(self):
+        return f"{self.video.title} - Chunk {self.chunk_number}"
--- link/tests.py
+++ link/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+class VideoAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_video(self):
+        # Provide the required fields in the request data
+        data = {
+            "title": "Test Video",
+            "description": "Test Description",
+            "Video_file": ''  # Use an empty string as a placeholder
+        }
+        response = self.client.post('/api/create_video/', data)
+        print(response.status_code)
+        print(response.content)
+        print(response.headers)
+        self.assertEqual(response.status_code, 201)
